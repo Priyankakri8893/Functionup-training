1973d6f4fbed355deffa50e8cd1552d34c3e3fc7
Priyanka <priyankakri8893@gmail.com>
2023-04-19 22:53:04 +0530

    problem 3 solve

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,12 +4,16 @@ const router = express.Router();
 const commonFile = require('./common')
 const loggerfile = require('../logger/logger')
 const utilFile = require('../util/helper')
+const formatterFile = require('../validator/formatter');
 
 router.get('/test-me', function (req, res) {
     loggerfile.welcome();
     utilFile.printDate();
     utilFile.printMonth();
     utilFile.getBatchInfo();
+    console.log(formatterFile.trimmedStr);
+    console.log(formatterFile.changetoLowerCase);
+    console.log(formatterFile.changeToUpperCase);
     res.send('This should be working!')
 });
 
