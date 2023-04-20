795498250786d77f002bebb6fa660fb4326dcc96
Priyanka <priyankakri8893@gmail.com>
2023-04-20 21:28:41 +0530

    problem 4 solve

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,6 +5,8 @@ const commonFile = require('./common')
 const loggerfile = require('../logger/logger')
 const utilFile = require('../util/helper')
 const formatterFile = require('../validator/formatter');
+const lodash = require('../lodash_/lodash_')
+
 
 router.get('/test-me', function (req, res) {
     loggerfile.welcome();
@@ -14,6 +16,11 @@ router.get('/test-me', function (req, res) {
     console.log(formatterFile.trimmedStr);
     console.log(formatterFile.changetoLowerCase);
     console.log(formatterFile.changeToUpperCase);
+
+    console.log(lodash.splitArr);
+    console.log(lodash.oddLastNinelm);
+    console.log(lodash.uniqueArr);
+    console.log(lodash.keyValuePair);
     res.send('This should be working!')
 });
 
