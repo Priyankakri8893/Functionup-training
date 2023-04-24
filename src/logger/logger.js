35b790062a93809dee5912258333e2b8cb817f0b
Priyanka <priyankakri8893@gmail.com>
2023-04-24 21:29:27 +0530

    assignment question add in readme

--- src/logger/logger.js
+++
@@ -1,6 +0,0 @@
-const welcome = function () {
-    const welcomeMsg = `Welcome to my application. I am Priyanka kumari and a part of FunctionUp Californium cohort.`
-    console.log(welcomeMsg);
-}
-
-module.exports = welcome();
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const loggerfile = require('../logger/logger')
 
 router.get('/test-me', function (req, res) {
-    console.log(loggerfile.welcome);
-    loggerfile.welcome();
     res.send('This should be working!')
 });
 
