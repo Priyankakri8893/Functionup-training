d87f5400f5ee3141c49ae97c945e377a6ed881ac
Priyanka Kumari <126274631+Priyankakri8893@users.noreply.github.com>
2023-05-01 08:59:55 +0530

    I remove my own string

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://priyankakri8893:8DX39VAJBvKPQmX0@priyankakumari.uvh8zli.mongodb.net/priyanka", {
+mongoose.connect("add here your own strings", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
